8a226d739c9f44b96818ed01c1dc4e38aa6bdb75
Akash-Kotla <101693455+Akash-Kotla@users.noreply.github.com>
2023-08-02 17:56:33 +0530

    initial commit

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.telecom.Call
+import com.example.practice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
